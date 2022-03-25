68090b37a8a358a03fadc6b94c6fc5527b03731e
balayogesh <balayogesh2000@gmail.com>
2022-03-25 13:37:00 +0530

    modified getalldoctors

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,6 +1,20 @@
 const Doctor = require("../models/doctorModel");
 const factory = require("./handlerFactory");
+const catchAsync = require("../utils/catchAsync");
 
+exports.getAllDoctors = catchAsync(async (req, res, next) => {
+  const params = {};
+  if (req.query.name) params.name = req.query.name;
+  if (req.query.speciality) params.speciality = req.query.speciality;
+  const doctors = await Doctor.find(params);
+  res.status(200).json({
+    status: "success",
+    data: {
+      results: doctors.length,
+      data: doctors
+    }
+  });
+});
 exports.getAllDoctors = factory.getAll(Doctor);
 exports.getDoctor = factory.getOne(Doctor);
 exports.createDoctor = factory.createOne(Doctor);
